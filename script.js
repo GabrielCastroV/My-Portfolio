8c3c494aa87f468c4f502ac5c5d20d310b41d91f
Gabriel <gabrielenriquecas@gmail.com>
2023-09-28 20:46:39 -0400

    panel added

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-window.addEventListener('DOMContentLoaded', function () {
-  // Smooth Scroll
-  const navLinks = document.querySelectorAll('nav a');
+const miniNavBar = document.querySelector('#mini-nav');
+const panelMobile = document.querySelector('.panel');
+const hamburguer = miniNavBar.children[0];
 
-  for (const link of navLinks) {
-    link.addEventListener('click', smoothScroll);
+hamburguer.addEventListener('click', () => {
+  if (!hamburguer.classList.contains('active')) {
+    hamburguer.classList.add('active');
+    hamburguer.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />'
+    panelMobile.classList.remove('hidden');
+  } else {
+    hamburguer.classList.remove('active');
+    hamburguer.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />'
+    panelMobile.classList.add('hidden');
   }
-
-  function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute('href');
-    document.querySelector(targetId).scrollIntoView({
-      behavior: 'smooth'
-    });
-  }
-});
+})
