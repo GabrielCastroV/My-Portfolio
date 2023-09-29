4c4e538dd235d8772f264fb8fef73d7f9b252299
Gabriel <gabrielenriquecas@gmail.com>
2023-09-29 17:17:59 -0400

    panel added

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const miniNavBar = document.querySelector('#mini-nav');
 const panelMobile = document.querySelector('.panel');
 const hamburguer = miniNavBar.children[0];
 
-hamburguer.addEventListener('click', () => {
-  if (!hamburguer.classList.contains('active')) {
-    hamburguer.classList.add('active');
-    hamburguer.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />'
-    panelMobile.classList.remove('hidden');
-  } else {
-    hamburguer.classList.remove('active');
-    hamburguer.innerHTML = '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />'
-    panelMobile.classList.add('hidden');
+panelMobile.addEventListener('click', () => {
+    if (hamburguer.classList.contains('active')) {
+        hamburguer.classList.remove('active');
+        panelMobile.classList.add('hidden');
+        miniNavBar.style.backgroundColor = 'transparent'
+    }
+})  
+
+hamburguer.addEventListener('click', e => {
+    e.preventDefault();
+    if (!hamburguer.classList.contains('active')) {
+        hamburguer.classList.add('active');
+        panelMobile.classList.remove('hidden');
+        miniNavBar.style.backgroundColor = '#5ed0b7ea'
+    } else {
+        hamburguer.classList.remove('active');
+        panelMobile.classList.add('hidden');
+        miniNavBar.style.backgroundColor = 'transparent'
   }
 })
